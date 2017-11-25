877eeb7776ef7e209985bd88903a4b426e8593e1
Gio d'Amelio <giodamelio@gmail.com>
2017-11-24 17:49:39 -0800

    Add start and end padThai varieties

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const pad = require('lodash.pad');
+const padStart = require('lodash.padstart');
+const padEnd = require('lodash.padend');
 
 module.exports.padThai = function(string, length) {
   return pad(string, length, 'thai');
+};
+
+module.exports.padThaiStart = function(string, length) {
+  return padStart(string, length, 'thai');
+};
+
+module.exports.padThaiEnd = function(string, length) {
+  return padEnd(string, length, 'thai');
 };
--- test/pad-thai.spec.js
+++ test/pad-thai.spec.js
@@ -1,9 +1,25 @@
-const { padThai } = require('../');
+const { padThai, padThaiStart, padThaiEnd } = require('../');
 
 describe('padThai()', () => {
   it('Pad correctly', () => {
     expect.assertions(1);
 
     expect(padThai('-AAA-', 22)).toBe('thaithai-AAA-thaithait');
+  });
+});
+
+describe('padThaiStart()', () => {
+  it('Pad correctly', () => {
+    expect.assertions(1);
+
+    expect(padThaiStart('-AAA-', 22)).toBe('thaithaithaithait-AAA-');
+  });
+});
+
+describe('padThaiEnd()', () => {
+  it('Pad correctly', () => {
+    expect.assertions(1);
+
+    expect(padThaiEnd('-AAA-', 22)).toBe('-AAA-thaithaithaithait');
   });
 });
