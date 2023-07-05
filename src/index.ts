7ed7b48d1b5b139d38e1782ced34d0c651a74b05
Gio d'Amelio <giodamelio@gmail.com>
2023-07-05 21:19:45 +0000

    Update Prettier and format codebase

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import {pad, padStart, padEnd} from 'lodash';
+import { pad, padStart, padEnd } from 'lodash';
 
 export function padThai(input: string, length: number) {
   return pad(input, length, 'thai');
-};
+}
 
 export function padThaiStart(input: string, length: number) {
   return padStart(input, length, 'thai');
-};
+}
 
 export function padThaiEnd(input: string, length: number) {
   return padEnd(input, length, 'thai');
-};
+}
